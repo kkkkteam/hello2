876ac6bb5560fad1b7c125a61f5453e392f023a7
ngtung <kntnov11@gmail.com>
2022-08-17 00:00:35 +0800

    solve the error

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -2,18 +2,18 @@
 pragma solidity >=0.4.17 <0.9.0;
 
 contract Migrations {
+
   address public owner = msg.sender;
   uint public last_completed_migration;
 
-  modifier restricted() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner."
-    );
-    _;
+  modifier restricted {
+
+    require(msg.sender == owner, "Only contract owner can do this");
+    _; 
   }
 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
   }
+
 }
